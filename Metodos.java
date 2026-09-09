9353ad38d2edcd49dfbb22192a94b680a44f221e
saraselene-dev <saraselene@gmail.com>
2026-09-08 21:58:37 -0500

    Desarrollo método registro de autos.

--- Metodos.java
+++ Metodos.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+
 public class Metodos {
+    public Stack<Objautos> RegistrarAutos(Stack<Objautos> pila, Scanner sc){
+        boolean  continuar = true;
+        while (continuar){
+            Objautos auto = new Objautos();
+            System.out.println("Ingrese la marca del vehículo");
+            auto.setMarca(sc.next());
+            System.out.println("Ingrese el modelo del vehiculo");
+            auto.setModelo(sc.next());
+            System.out.println("Ingrese el año del vehiculo");
+            auto.setAnio(sc.nextInt());
+            System.out.println("ingrese el estado del vechiulo (Nuevo/Usado)");
+            auto.setEstado(sc.next());
+            System.out.println("Disponible para entrega inmediante 1) Si / 2) No");
+            int n=sc.nextInt();
+            if (n == 1) {
+                auto.setTiempoEspera(0);
+                
+            } else {
+
+                System.out.println("Ingrese los dias de entrega.");
+                auto.setTiempoEspera(sc.nextInt());
+            }
+
+        }
+        return pila;
+    }
     
 }
