dfb7d84f8e7ed9e224557f9d415fa9260b7118d0
Juan David <tojuanchot@gmail.com>
2026-09-09 08:10:29 -0500

    Creación clase Menu para ejecutar pruebas

--- Menu.java
+++ Menu.java
@@ -1,3 +1,41 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Menu {
-    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        boolean continuar = true;
+        int opt;
+        Metodos m = new Metodos();
+        Stack<Objautos> pila = new Stack<Objautos>();
+
+            while(continuar){
+
+                System.out.println("Concesionario Estructuras S&J");
+                System.out.println("-----------------------");
+
+                System.out.println("1. Registrar vehículo.");
+                System.out.println("0. Salir.");
+                System.out.println("Ingrese una opción: ");
+                opt = sc.nextInt();
+                sc.nextLine();
+                System.out.println("-----------------------");
+
+
+                switch (opt) {
+                    case 1:
+                        pila = m.RegistrarAutos(pila, sc);
+                        break;
+                
+                    default:
+                        break;
+                }
+
+
+            }
+
+
+        sc.close();
+    }
 }
